0d43dbb0fdc8fe58bf0b844b1813e566179e0dca
Danilo Raniery <danilo.coutinho@virtus.ufcg.edu.br>
2022-06-23 22:35:46 -0300

    Commit inicial de organização do projeto.

---
+++ src/main/java/com/bomdestino/sgm/SGMApplication.java
@@ -0,0 +1,13 @@
+package com.bomdestino.sgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SGMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SGMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bomdestino/sgm/SGMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bomdestino.sgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SGMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
